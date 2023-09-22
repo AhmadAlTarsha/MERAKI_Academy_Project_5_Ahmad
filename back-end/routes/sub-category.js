13d043735c9ac90b14d61b21b8a51a085ec4da22
MohMajali <mmajali45@gmail.com>
2023-09-23 02:29:26 +0300

    Added All Categories Functions

---
+++ back-end/routes/sub-category.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const subCategoryRouter = express.Router();
+const {
+  addSubCategory,
+  updateSubCategory,
+  getAllSubCategoriesOnCategory,
+  getSubCateogoryById,
+} = require("../controllers/sub-category");
+
+const { authentication } = require("../middlewares/authentication");
+const { authorization } = require("../middlewares/authorization");
+
+subCategoryRouter.post(
+  "/",
+  authentication,
+  authorization("CATEGORY-CONTROL"),
+  addSubCategory
+);
+
+subCategoryRouter.put(
+  "/:id",
+  authentication,
+  authorization("CATEGORY-CONTROL"),
+  updateSubCategory
+);
+
+subCategoryRouter.get(
+  "/:categoryId",
+  authentication,
+  authorization("CATEGORY-CONTROL"),
+  getAllSubCategoriesOnCategory
+);
+
+subCategoryRouter.get(
+  "/category/:id",
+  authentication,
+  authorization("CATEGORY-CONTROL"),
+  getSubCateogoryById
+);
+
+module.exports = subCategoryRouter;
