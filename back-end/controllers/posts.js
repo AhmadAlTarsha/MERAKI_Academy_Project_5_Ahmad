d4bf0dcc4148598955475842ec0d602fa1f74ce3
IbrahimHushki <hushki.ibrahim@gmail.com>
2023-09-24 21:57:52 +0300

    Updated posts functions and routes

--- back-end/controllers/posts.js
+++ back-end/controllers/posts.js
@@ -3,18 +3,42 @@ const { throwError } = require("../middlewares/throwError");
 
 // ===================== GET ALL POSTS =====================
 exports.getAllPosts = (req, res, next) => {
-  const { active } = req.body;
-  const query = `SELECT * FROM posts WHERE active = $1;`;
+  let images = [];
+  let posts = [];
+  const { active } = req.params;
+  const query = `SELECT posts.id, posts.title, posts.description, posts.category_id, posts.sub_category_id, posts.created_at, users.first_name, users.last_name, users.image FROM posts JOIN users ON users.id = posts.poster_id WHERE posts.active = $1`;
   pool
     .query(query, [active])
     .then((result) => {
+      console.log(active);
       if (result.command === `SELECT`) {
-        return res.status(200).json({
-          error: false,
-          posts: result.rows,
-        });
+        posts = result.rows;
+        const newQuery = `SELECT * FROM serverices_images`;
+        return pool.query(newQuery);
       }
       return throwError(400, "Something went wrong");
+    })
+    .then((result2) => {
+      images = result2.rows;
+      posts = posts.map((post) => ({
+        id: post.id,
+        user: {
+          fullName: `${post.first_name} ${post.last_name}`,
+          userImage: post.image,
+        },
+        title: post.title,
+        description: post.description,
+        category_id: post.category_id,
+        sub_category_id: post.sub_category_id,
+        created_at: post.created_at,
+        images: images.filter((image) => {
+          return image.service_id === post.id;
+        }),
+      }));
+      return res.status(200).json({
+        error: false,
+        posts: posts, // each post has images key
+      });
     })
     .catch((err) => {
       if (!err.statusCode) {
@@ -49,24 +73,59 @@ exports.getAllPostsByUser = (req, res, next) => {
 
 // ===================== CREATE NEW POST =====================
 exports.createPost = async (req, res, next) => {
-  const { title, description, main_image, category_id, sub_category_id } =
-    req.body;
-  const data = [title, description, main_image, category_id, sub_category_id];
-
-  const query = `INSERT INTO posts (title, description,main_image,
+  const { id } = req.token.user;
+  const {
+    title,
+    description,
+    main_image,
+    images,
     category_id,
-    sub_category_id,) VALUES ($1, $2, $3, $4, $5)`;
+    sub_category_id,
+  } = req.body;
+  const data = [
+    id,
+    title,
+    description,
+    main_image,
+    category_id,
+    sub_category_id,
+  ];
+
+  const query = `INSERT INTO posts (poster_id, title, description,main_image,
+    category_id,
+    sub_category_id) VALUES ($1, $2, $3, $4, $5, $6)`;
 
   pool
     .query(query, data)
     .then((result) => {
       if (result.rowCount !== 0) {
-        return res.status(201).json({
+        if (images.length === 0) {
+          res.status(201).json({
+            error: false,
+            message: "New post created",
+          });
+        } else {
+          return pool.query(`SELECT id FROM posts ORDER BY id DESC LIMIT 1`);
+        }
+      }
+      return throwError(400, "Something went wrong");
+    })
+    .then(async (result2) => {
+      try {
+        const newPostId = result2.rows[0].id;
+        for (let i = 0; i < images.length; i++) {
+          await pool.query(
+            `INSERT INTO serverices_images (service_id, image) VALUES ($1, $2)`,
+            [newPostId, images[i]]
+          );
+        }
+        res.json({
           error: false,
           message: "New post created",
         });
+      } catch (err) {
+        throw err;
       }
-      return throwError(400, "Something went wrong");
     })
     .catch((err) => {
       if (!err.statusCode) {
