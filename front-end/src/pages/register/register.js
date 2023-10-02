7d36739e033275a9fc53d65ec7d6d239d719efb1
MohMajali <mmajali45@gmail.com>
2023-10-02 15:32:51 +0300

    Changes on front end

--- front-end/src/pages/register/register.js
+++ front-end/src/pages/register/register.js
@@ -141,7 +141,7 @@ export const Register = () => {
                                 <label
                                for="profile-image" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Profile Image</label>
                                 <input onChange={(e)=>{
-                                    settest(e.target)
+                                    // settest(e.target)
                                     console.log(e.target.files[0]);
                                 }}  type="file" name="profile-image" id="profile-image" accept="image/*" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                             </div>
