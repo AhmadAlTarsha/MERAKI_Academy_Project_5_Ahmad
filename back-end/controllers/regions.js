0088e38bc809cbe8b0a7f07c6ea427f1e0f7ad05
MohMajali <mmajali45@gmail.com>
2023-10-02 19:03:25 +0300

    updates 2

--- back-end/controllers/regions.js
+++ back-end/controllers/regions.js
@@ -1,45 +1,40 @@
 const pool = require("../models/DB");
 const { throwError } = require("../middlewares/throwError");
 
+exports.getAllRegions = async (req, res, next) => {
+  const query = `SELECT * FROM regions  `;
+  try {
+    const response = await pool.query(query);
 
-
-exports.getAllRegions=async(req,res,next)=>{
-
-    const query=`SELECT * FROM regions  `
-    try {
-   const response=await pool.query(query)  
-  
     res.status(200).json({
-        error: false,
-        message: "All regions",
-        regions: response.rows,
-    })
-console.log(response);
-} catch (error) {
+      error: false,
+      message: "All regions",
+      regions: response.rows,
+    });
+    console.log(response);
+  } catch (error) {
     if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-}
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
 
-}
-
-exports.addNewRegions=async(req,res,next)=>{
-const {region}=req.body
-const value=[region]
-const query=`INSERT INTO regions (region) VALUES ($1)`
-try {
-   const response=await pool.query(query,value)
-   res.status(200).json({
-    error: false,
-    message: "regions added ",
-    regions: response.rows,
-  });
-} catch (err) {
+exports.addNewRegions = async (req, res, next) => {
+  const { region } = req.body;
+  const value = [region];
+  const query = `INSERT INTO regions (region) VALUES ($1)`;
+  try {
+    const response = await pool.query(query, value);
+    res.status(200).json({
+      error: false,
+      message: "regions added ",
+      regions: response.rows,
+    });
+  } catch (err) {
     if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-}
-
-}
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
