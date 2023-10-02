1002102e82180eba1c1868ad01fe78120763ce91
IbrahimHushki <hushki.ibrahim@gmail.com>
2023-10-02 23:58:55 +0300

    Added pagination to posts frontend & backend

--- front-end/src/Services/APIS/Posts/GetAllPosts.js
+++ front-end/src/Services/APIS/Posts/GetAllPosts.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 
-export const GetAllPosts = async () => {
+export const GetAllPosts = async (limit, offset, isDeleted) => {
+  console.log(limit, offset, isDeleted);
   try {
-    const result = await axios.get(`http://localhost:5000/posts/post/0`);
-    if (!result.data.error) {
-      return result.data.posts;
-    } 
-    
+    const result = await axios.get(
+      `http://localhost:5000/posts/post/?limit=${limit}&offset=${offset}&is_deleted=${isDeleted}`
+    );
+    if (!result?.data?.error) {
+      return result?.data?.posts;
+    }
   } catch (err) {
+    console.log("ERROR ==> ", err);
     throw err;
   }
 };
