e6d0481e51e0b3f441c4aa17e27ee6c1cdbcef68
AhmadALTarsha <ahmadwael.altarsha@gmail.com>
2023-10-04 14:45:04 +0300

    update

--- front-end/src/components/Navbars/Navbar.js
+++ front-end/src/components/Navbars/Navbar.js
@@ -98,11 +98,11 @@ const Navbar = () => {
               <NavLink to={"login"}>Login</NavLink>
               <br ></br>
               <NavLink to={"logout"}>Logout</NavLink>
-              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Company</a>
+              <br ></br>
+              <NavLink to={"/"}>Home</NavLink>
+             
             </div>
-            <div class="py-6">
-            <NavLink to={"login"}>Login</NavLink>
-            </div>
+           
           </div>
         </div>
       </div>
