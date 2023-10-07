045792066b6c774cb06a91b5392ec23b44e0c79f
AhmadALTarsha <ahmadwael.altarsha@gmail.com>
2023-10-07 06:02:15 +0300

    activate add new post form

--- front-end/src/components/New_Post/NewPost.js
+++ front-end/src/components/New_Post/NewPost.js
@@ -30,20 +30,26 @@ function NewPost({
 
 }) {
   const [newService, setNewService] = useState({});
+  const [newPost, setNewPost] = useState({
+   
+  });
 
   const handleChange = (e) => {
-    console.log(newService);
+    !toggle?
     setNewService({
       ...newService,
       [e.target.name]: e.target.name === "image" ? e.target.files[0] : e.target.value
 
-    });
+    }):setNewPost({
+      ...newPost,
+      [e.target.name]: e.target.name === "image" ? e.target.files[0] : e.target.value
+
+    })
   };
-  const handleNewService = (e) => {
+  const handleNewServiceOrPOST = (e) => {
     e.preventDefault();
-    // console.log("SERIVE NEW ===> ", newService);
-    addNewServices(newService)
-      .then((result) => {
+    if (toggle) {
+      CreateNewPost(newPost).then((result) => {
         console.log(result);
 
       })
@@ -54,6 +60,21 @@ function NewPost({
       .finally(() => {
         setLoading(false);
       });
+    }else{
+   
+       addNewServices(newService)
+      .then((result) => {
+        console.log(result);
+
+      })
+      .catch((err) => {
+
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });}
+   
   };
   const selectcategory = useSelector((state) => {
     return {
@@ -61,23 +82,6 @@ function NewPost({
       subcategories: state?.subCategories.subCategories
     };
   });
-
-  const handleNewPost = async () => {
-
-
-
-
-    if (toggle) {
-      CreateNewPost(description, images, category_id, sub_category_id)
-        .then((res) => { })
-        .catch((err) => {
-          console.log(err);
-        });
-    } else { addNewServices(description, images, category_id, sub_category_id,) }
-
-
-  };
-
   return (
     <>
       {toggle ? <><div className="heading text-center font-bold text-2xl m-5 text-gray-800">
@@ -88,42 +92,64 @@ function NewPost({
         <div className="editor mx-auto w-10/12 flex flex-col text-gray-800 border border-gray-300 p-4 shadow-lg max-w-2xl">
           <div>
             <label for="category">Category</label>
-            <select name="category">
-              <option value="plumbing">Plumbing</option>
-              <option value="electricity">Electricity</option>
+            <select onClick={(e)=>{
+              handleChange(e)
+               GetSubCategoriesOnCategory(e?.target?.value)
+               .then((result) => {
+                 // console.log(result);
+                 dispatch(setSubCategories(result));
+
+               }).catch((err) => {
+                 console.log(err);
+               })
+            }}
+             name="category_id">
+              {/* <option value="plumbing">Plumbing</option>
+              <option value="electricity">Electricity</option> */}
+               <option value="" disabled selected>
+                Select categories
+              </option>
+              {selectcategory?.categories?.map((cat, i) => {
+                return <option key={i} value={cat.id}>{cat.name}</option>
+              })}
             </select>
           </div>
           <div>
             <label for="Subcategory">Subcategory</label>
-            <select name="Subcategory">
-              <option value="plumbing">Plumbing</option>
-              <option value="electricity">Electricity</option>
+            <select onClick={(e)=>{
+              handleChange(e)
+              console.log(e.target.value);
+            }} name="sub_category_id">
+              {/* <option value="plumbing">Plumbing</option>
+              <option value="electricity">Electricity</option> */}
+                <option disabled value="">select sub_category</option>
+              {selectcategory?.subcategories?.subCategories?.map((sub_cat, i) => {
+                return <option key={i} value={sub_cat?.id}>{sub_cat.name}</option>
+              })}
             </select>
           </div>
 
           <textarea
+          onChange={(e)=>handleChange(e)}
             className="description bg-gray-100 sec p-3 h-60 border border-gray-300 outline-none mt-2"
             spellcheck="false"
             placeholder="Write your post here"
+            name="description"
           >
             {postBody}
           </textarea>
-          <div class="icons flex text-gray-500 m-2">
-            <svg
-              class="mr-2 cursor-pointer hover:text-gray-700 border rounded-full p-1 h-7"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
-              />
-            </svg>
-          </div>
+          <input
+            onChange={(e) => {
+            
+              handleChange(e)
+            }}
+            type="file"
+            name="image"
+            id="profile-image"
+            accept="image/*"
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required=""
+          />
           <div className="buttons flex">
             <div className="btn border border-gray-300 p-1 px-4 font-semibold cursor-pointer text-gray-500 ml-auto">
               <Button
@@ -132,7 +158,10 @@ function NewPost({
               ></Button>
             </div>
             <div className="btn border border-indigo-500 p-1 px-4 font-semibold cursor-pointer text-gray-200 ml-2 bg-indigo-500">
-              <Button onClick={handleNewPost} buttonName={"Post"}></Button>
+              <Button onClick={(e)=>{
+                console.log(newPost);
+              handleNewServiceOrPOST(e)
+              }}  buttonName={"Post"}></Button>
             </div>
           </div>
         </div></> : <><div className="heading text-center font-bold text-2xl m-5 text-gray-800">
@@ -160,21 +189,22 @@ function NewPost({
                 Select categories
               </option>
               {selectcategory.categories.map((cat, i) => {
-                return <option key={i} value={cat.id}>{cat.name}</option>
+                return <option key={i} value={cat?.id}>{cat.name}</option>
               })}
 
-              {/* 
-            <option value="electricity">Electricity</option> */}
+               
+            
             </select>
           </div>
           <div>
             <label for="Subcategory">Sub_category</label>
-            <select onChange={(e) => {
+            <select onClick={(e)=>{
               handleChange(e)
+              console.log(e.target.value);
             }} name="sub_category_id">
               <option disabled value="">select sub_category</option>
               {selectcategory?.subcategories?.subCategories?.map((sub_cat, i) => {
-                return <option key={i} value={sub_cat.id}>{sub_cat.name}</option>
+                return <option key={i} value={sub_cat?.id}>{sub_cat.name}</option>
               })}
             </select>
           </div>
@@ -200,22 +230,6 @@ function NewPost({
           >
             {postBody}
           </textarea>
-          {/* <div class="icons flex text-gray-500 m-2">
-            <svg
-              class="mr-2 cursor-pointer hover:text-gray-700 border rounded-full p-1 h-7"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
-              />
-            </svg>
-          </div> */}
           <input
             onChange={(e) => {
               console.log(e.target.files[0]);
@@ -243,7 +257,7 @@ function NewPost({
               ></Button>
             </div>
             <div className="btn border border-indigo-500 p-1 px-4 font-semibold cursor-pointer text-gray-200 ml-2 bg-indigo-500">
-              <Button onClick={handleNewService} buttonName={"Add"}></Button>
+              <Button onClick={handleNewServiceOrPOST} buttonName={"Add"}></Button>
             </div>
           </div>
         </div></>}
--- front-end/src/pages/Home/Home.js
+++ front-end/src/pages/Home/Home.js
@@ -14,7 +14,7 @@ import Comment from "../../components/Comment/Comment";
 import { GetCategories } from "../../Services/APIS/Category/Get_Categories";
 import { setCategories } from "../../Services/Redux/Category";
 import { setSubCategories } from "../../Services/Redux/Sub_Categories";
-import { setServices } from "../../Services/Redux/Services";
+import { setServices } from "../../Services/Redux/services";
 
 import Categories from "../../components/Home_Categories/Categories";
 import Sub_Categories from "../../components/Home_Categories/Sub_Categories";
@@ -211,43 +211,7 @@ const Home = () => {
               <Tabs setToggle={setToggle} />
 
 
-              <div>
-                <ul
-                  class="mb-4 flex list-none flex-row flex-wrap border-b-0 pl-0"
-                  id="tabs-tab3"
-                  role="tablist"
-                  data-te-nav-ref
-                >
-                  <div>
-                    <li role="presentation">
-                      <Button
-                        buttonName={"services"}
-                        buttonClassName={
-                          "my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
-                        }
-                        onClick={() => {
-                          setToggle(false);
-                          console.log(servicessSelector.services);
-                        }}
-                      ></Button>
-                    </li>
-                  </div>
-
-                  <li role="presentation">
-                    <Button
-                      buttonName={"AllPosts"}
-                      buttonClassName={
-                        "my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
-                      }
-                      onClick={() => {
-                        setToggle(true);
-                      }}
-                    ></Button>
-                  </li>
-                </ul>
-              </div>
-
-              <NewPost
+                <NewPost
                 toggle={toggle}
                 isCategoryClicked={isCategoryClicked}
                 dispatch={dispatch}
