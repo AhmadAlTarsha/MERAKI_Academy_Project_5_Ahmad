1d151fb1f3d0cf1dd0b1cc822e51ba5c38078a7d
MohMajali <mmajali45@gmail.com>
2023-10-07 18:39:10 +0300

    hello from the other side

--- front-end/src/pages/Home/Home.js
+++ front-end/src/pages/Home/Home.js
@@ -205,56 +205,17 @@ const Home = () => {
                 />
               )}
 
-
               {/* <TAP></TAP> */}
 
               <Tabs setToggle={setToggle} />
-
-
-              <div>
-                <ul
-                  class="mb-4 flex list-none flex-row flex-wrap border-b-0 pl-0"
-                  id="tabs-tab3"
-                  role="tablist"
-                  data-te-nav-ref
-                >
-                  <div>
-                    <li role="presentation">
-                      <Button
-                        buttonName={"services"}
-                        buttonClassName={
-                          "my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
-                        }
-                        onClick={() => {
-                          setToggle(false);
-                          console.log(servicessSelector.services);
-                        }}
-                      ></Button>
-                    </li>
-                  </div>
-
-                  <li role="presentation">
-                    <Button
-                      buttonName={"AllPosts"}
-                      buttonClassName={
-                        "my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
-                      }
-                      onClick={() => {
-                        setToggle(true);
-                      }}
-                    ></Button>
-                  </li>
-                </ul>
-              </div>
 
               <NewPost
                 toggle={toggle}
                 isCategoryClicked={isCategoryClicked}
                 dispatch={dispatch}
-                setError={setError} 
+                setError={setError}
                 setLoading={setLoading}
               />
-
 
               {toggle
                 ? select?.post.map((newPost) => {
@@ -312,9 +273,7 @@ const Home = () => {
                       </>
                     );
                   })
-
                 : servicessSelector?.services?.map((service) => {
-
                     return (
                       <>
                         <Post
